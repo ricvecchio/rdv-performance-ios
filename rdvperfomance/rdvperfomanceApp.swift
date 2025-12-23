58d86dc72da84e7b191c168b5ae3d28f83da3f73
ricardodelvecchio <ric.vecchio@gmail.com>
2025-12-23 11:56:21 -0300

    Initial Commit

---
+++ rdvperfomance/rdvperfomanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  rdvperfomanceApp.swift
+//  rdvperfomance
+//
+//  Created by Ricardo Del Vecchio on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct rdvperfomanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
